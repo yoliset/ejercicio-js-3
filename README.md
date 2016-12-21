0c576fbbeb115919e44f093b5172f5ac117fe8bc
Your Name <you@example.com>
2016-12-21 00:46:01 -0500

    project completed

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Ejercicio3
 
-##Inicio : Calculo del Sueldo Semanal
+Inicio : Calculo del Sueldo Semanal
 
-##Ingreso de Datos
+## Ingreso datos
 - Sueldo_Semanal
 - Sueldo_Hora
 - Cant_Horas
 
 
-##Proceso
+## Proceso
 - Pedir Sueldo por Hora
 - Pedir La cantidad de horas
 - Multiplicar  Sueldo_Hora por Cant_Horas y alamacenar en Sueldo_Semanal
 - Mostrar Sueldo_Semanal
 
-##Fin
+*Fin*
--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var sueldo_Hora=null;
 var cant_Horas=null;
 
 
-sueldo_Hora=prompt("Por favor ingresa el sueldo que ganas por hora");
-cant_Horas=prompt("Por favor ingresa la cantidad de horas que trabajas a la semana");
+sueldo_Hora = prompt("Por favor ingresa el sueldo que ganas por hora");
+cant_Horas = prompt("Por favor ingresa la cantidad de horas que trabajas a la semana");
 
 sueldo_Semanal=(parseFloat(sueldo_Hora)*parseFloat(cant_Horas));
 alert(sueldo_Semanal);
